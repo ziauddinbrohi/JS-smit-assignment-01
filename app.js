e117fd7b0b607657d5957e4c6c2cb1fce14a9209
Muhammad Ziauddin Brohi <155188806+ziauddinbrohi@users.noreply.github.com>
2024-08-23 11:44:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// JS Assignment 01 (alert)
+
+// Question 01
+// alert("Hello, Welcome to our Website")
+// Question 02
+// alert("Error! Please enter a valid password")
+// Question 03
+// alert("Welcome to JS land...\nHappy Codding!")
+// Question 04
+// alert("Welcome to JS land...")
+// alert("Happycoding!")
+// Question 05
+// alert("Hello...I can run JS through my web browser's console")
+
+// JS Assignment 02 (var)
+var username;
+var myname=Ziauddinbrohi;
+
+
+
